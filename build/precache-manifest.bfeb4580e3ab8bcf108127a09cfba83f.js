8e673b1dcdfab61a3c8e6a95a30605c685914772
sonidiaz <nachodiaz8@gmail.com>
2021-10-06 20:24:41 +0200

    Add dist folder

---
+++ build/precache-manifest.bfeb4580e3ab8bcf108127a09cfba83f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dd19871564fafd59831b3369d3b2c21",
+    "url": "/nachodiaz/index.html"
+  },
+  {
+    "revision": "5fba3ee5abe311c28394",
+    "url": "/nachodiaz/static/js/2.c7acdaad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/nachodiaz/static/js/2.c7acdaad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "770f421cf4ffeec0ad84",
+    "url": "/nachodiaz/static/js/main.272dbea6.chunk.js"
+  },
+  {
+    "revision": "d9cda4239d2837c82235",
+    "url": "/nachodiaz/static/js/runtime-main.124359c5.js"
+  },
+  {
+    "revision": "fa719f42c9cd0a98f584ba45b911f9c9",
+    "url": "/nachodiaz/static/media/010-linkedin.fa719f42.png"
+  },
+  {
+    "revision": "f4ab5cc172d0912ac76f48d55e67a035",
+    "url": "/nachodiaz/static/media/013-twitter-1.f4ab5cc1.png"
+  },
+  {
+    "revision": "7201b8918cd41c0ecc73bf3652d62e23",
+    "url": "/nachodiaz/static/media/fullscreen-shopmaster.7201b891.png"
+  },
+  {
+    "revision": "9c8ba6128b47b0f66d5a4d9617f70676",
+    "url": "/nachodiaz/static/media/github.9c8ba612.png"
+  },
+  {
+    "revision": "26739d10bfc0fabd1a89bdb20933bbe1",
+    "url": "/nachodiaz/static/media/nachodiazv2.26739d10.png"
+  },
+  {
+    "revision": "fac6f67669cb223609306ebf4834968c",
+    "url": "/nachodiaz/static/media/screen-bfg-community.fac6f676.png"
+  },
+  {
+    "revision": "b787180ca22f7a964050c2eb8ffab804",
+    "url": "/nachodiaz/static/media/screen-bfg.b787180c.png"
+  },
+  {
+    "revision": "e97be41bd87d7a899e4ff2e13f3c9776",
+    "url": "/nachodiaz/static/media/screen-callao.e97be41b.png"
+  },
+  {
+    "revision": "05b98810d8ad379cb2b5e715131b3398",
+    "url": "/nachodiaz/static/media/screen-conotrosojos.05b98810.png"
+  },
+  {
+    "revision": "87db7adf77a60c7153a6baaf8990a203",
+    "url": "/nachodiaz/static/media/screen-fgc.87db7adf.png"
+  },
+  {
+    "revision": "39d127795b8165d7929f85a8ba9b0541",
+    "url": "/nachodiaz/static/media/screen-flateli.39d12779.jpg"
+  },
+  {
+    "revision": "5533d2f9350bb763d540757c86ff66b5",
+    "url": "/nachodiaz/static/media/screen-nomascuentos.5533d2f9.png"
+  },
+  {
+    "revision": "365b5f586b797f578613e2e6c29852b2",
+    "url": "/nachodiaz/static/media/screen-puma.365b5f58.png"
+  },
+  {
+    "revision": "a9898adda2625b5022f9c2217365fc6f",
+    "url": "/nachodiaz/static/media/screen-thairoom.a9898add.jpg"
+  },
+  {
+    "revision": "a4bf2034a3b32254e7dd327d2f23f4ef",
+    "url": "/nachodiaz/static/media/screen-weather.a4bf2034.png"
+  },
+  {
+    "revision": "c3aa59e22a6fed88c69dc0076cde3e6c",
+    "url": "/nachodiaz/static/media/site-huawei.c3aa59e2.jpg"
+  },
+  {
+    "revision": "583121f9289d20e4981adf09bd8e84a5",
+    "url": "/nachodiaz/static/media/user-icon-1.583121f9.png"
+  },
+  {
+    "revision": "6eec91f2da64db4c276f16c6b182e965",
+    "url": "/nachodiaz/static/media/user-icon-2.6eec91f2.png"
+  },
+  {
+    "revision": "923ddf5b03635c121e20316953269497",
+    "url": "/nachodiaz/static/media/user-icon-3.923ddf5b.png"
+  },
+  {
+    "revision": "cd9dc26cc78539339885c933be18710b",
+    "url": "/nachodiaz/static/media/user-icon-4.cd9dc26c.png"
+  },
+  {
+    "revision": "7ba182448a9aabfbab4fcd0bcafb0d6c",
+    "url": "/nachodiaz/static/media/wordpress.7ba18244.png"
+  }
+]);
